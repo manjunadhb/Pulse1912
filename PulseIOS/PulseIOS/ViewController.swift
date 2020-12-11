cb89481c878c23e97cfecc388fff0853aa9c0942
Manjunadh <manjunadhb@gmail.com>
2020-12-11 16:27:07 +0530

    Created a new pulse IOS Project.

---
+++ PulseIOS/PulseIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PulseIOS
+//
+//  Created by MacUser on 11/12/20.
+// Doing some changes
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
